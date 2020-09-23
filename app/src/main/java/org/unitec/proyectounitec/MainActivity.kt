679ac3b7cf48f5b12e80699b1a19d73a3b055bf1
Eduardoa97 <josuearrieta40@gmail.com>
2020-09-23 01:07:56 -0500

    Segundo intento

---
+++ app/src/main/java/org/unitec/proyectounitec/MainActivity.kt
@@ -0,0 +1,20 @@
+package org.unitec.proyectounitec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+//EL PUNTO DE PARTIDA DE TODA APP ES LA MAINACTIVYTI (PRESENTER)
+
+/*
+Public class TarjetaCredito extends Tarjeta{
+public TarjetaCredito(){
+super();
+}
+}
+ */
+class MainActivity : AppCompatActivity() {
+    //fun en kotlin es una palabra reservada para implementar o crear una funcion o metodo
+    override fun onCreate(nombre: Bundle?) {
+        super.onCreate(nombre)
+        setContentView(R.layout.activity_main)
+    }
+}
